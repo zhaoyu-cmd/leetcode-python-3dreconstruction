2f2eb458d3415a48fea4594d9815864791ad3272
zhaoyu-cmd <17537664417@163.com>
2026-02-25 21:09:48 +0800

    快慢指针，点云去重、图像特征点去重

---
+++ phase2_技术深化期/11月_ORB-SLAM2+3DGS基础/数组列表/删除有序数组的重复项.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        if not nums:
+            return 0
+        slow=0
+        for fast in range(1,len(nums)):
+            if nums[fast]!=nums[slow]:
+                slow+=1
+                nums[slow]=nums[fast]
+        return slow+1
